e1eba3e6913c7750de542afdebe9d18ef7caa855
Nikunj Sharma <sharmanicktop@gmail.com>
2023-09-22 11:31:16 +0530

    Final Commit

--- register.js
+++ register.js
@@ -1,6 +1,27 @@
 
 console.log("Form submit event triggered");
 
+const fileInput = document.getElementById("file-input");
+const previewImage = document.getElementById("preview-image");
+
+fileInput.addEventListener("change", function () {
+    const selectedFile = fileInput.files[0];
+
+    if (selectedFile) {
+        const reader = new FileReader();
+
+        reader.onload = function () {
+            previewImage.src = reader.result;
+            previewImage.style.display = "block";
+        };
+
+        reader.readAsDataURL(selectedFile);
+    } else {
+        previewImage.style.display = "none";
+    }
+});
+
+
 document.getElementById('registerForm').addEventListener('submit', function (event) {
     var firstName = document.getElementById('firstName').value;
     var lastName = document.getElementById('lastName').value;
